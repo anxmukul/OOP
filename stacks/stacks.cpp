6c815b4c259e92d983f7cf4da2d984704e48d7ae
Mukul <kmukul1197@gmail.com>
2020-08-23 10:08:38 +0530

    defining scopes for member functions

--- stacks/stacks.cpp
+++ stacks/stacks.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int a[10];
 int top = -1;
-void push_an_integer(int x){
+void Stack::push_an_integer(int x){
     if(top == 9){
         cout<<"Error: Stack Overflow";
         cout<<"exanding memory for storing data\n";
@@ -15,16 +15,16 @@ void push_an_integer(int x){
     top = top+1;
     a[top] = x;
 }
-void pop_an_interger(){
+void Stack::pop_an_interger(){
     if(top == -1){
         cout<<"Stack is Empty.";
         return;
     }
     top = top-1;
 }
-int get_top_element(){
+int Stack::get_top_element(){
     return a[top];
 }
-int length_of_stack(){
+int Stack::length_of_stack(){
     return top;
 }
--- stacks/stacks.h
+++ stacks/stacks.h
@@ -1,8 +1,9 @@
 // header file for stacks;
-class stack{
+class Stack
+{
     private:
     public:
-    void push_an_integer();
+    void push_an_integer(int x);
     void pop_an_interger();
     int get_top_element();
     int length_of_stack();
