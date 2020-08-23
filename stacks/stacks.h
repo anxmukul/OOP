1ad717dd9c38920ccdc1dfd7805a0ade752d8c25
Mukul <kmukul1197@gmail.com>
2020-08-23 09:54:25 +0530

    Creating header file and interface for users

---
+++ stacks/stacks.h
@@ -0,0 +1,9 @@
+// header file for stacks;
+class stack{
+    private:
+    public:
+    void push_an_integer();
+    void pop_an_interger();
+    int get_to_element();
+    int length_of_stack();
+};
