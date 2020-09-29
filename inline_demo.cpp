b1f1e36d0bc3984debbde26268f105341c2c5f55
Mukul <kmukul1197@gmail.com>
2020-09-29 10:42:50 +0530

    Inline function demo

---
+++ inline_demo.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+inline int MAX(int a, int b){
+    if(a>b)
+        return a;
+    else
+        return b;
+}/*
+int MAX(int a, int b){
+    if(a>b)
+        return a;
+    else
+        return b;
+}*/
+int main(){
+    cout<<"MAX(100, 300):\t"<<MAX(100, 300)<<endl;
+    return 0;
+}
