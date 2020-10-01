193c52169daace0aa3fd9ef2076385d5fe39709e
Mukul <kmukul1197@gmail.com>
2020-10-01 11:15:24 +0530

    Friends class: Fixing indentation

--- demo7.cpp
+++ demo7.cpp
@@ -26,7 +26,7 @@ public:
         cout << "Sports = " << sport << "\n";
         cout << "\n";
     }
-    friend class teacher;   //Teacher class id friend class of student 
+    friend class teacher; //Teacher class id friend class of student
 };
 int student::total; //Initializing static member outside class;
 class teacher
@@ -34,19 +34,24 @@ class teacher
 private:
     string name;
     int class_teacher;
+
 public:
-    void set_details(string s, int c){
+    void set_details(string s, int c)
+    {
         name = s;
         class_teacher = c;
     }
-    void top_student(student &t){
+    void top_student(student &t)
+    {
         t.get_details();
     }
-    void top_student_percentage(student &t){
-        cout<<"Percentage of first student\t"<<t.percentage<<endl;     //It can access private member of student class;
+    void top_student_percentage(student &t)
+    {
+        cout << "Percentage of first student\t" << t.percentage << endl; //It can access private member of student class;
     }
 };
-int main(){
+int main()
+{
     student s1;
     s1.set_details("Alex", 67.89, "Cricket");
     student s2;
