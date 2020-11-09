0a11667a72756ad100fed2739df50b1f6d3396d5
Mukul <kmukul1197@gmail.com>
2020-11-09 12:09:34 +0530

    Virtual funtion demo

---
+++ virtual_function.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Base{
+    public:
+    virtual void Print(){
+        cout<<"Print Base class details\n";
+    }
+    void show(){
+        cout<<"Show Base class Details\n";
+    }
+};
+class Derived: public Base{
+    public:
+    void Print(){
+        cout<<"Print Derived class details\n";
+    }
+    void show(){
+        cout<<"Show Derived class Details\n";
+    }
+};
+int main(){
+    Base *ptr;
+    Derived d;
+    ptr = &d;
+    ptr->Print();
+    ptr->show();    //Non-virtual function early binding occur; 
+    return 0;
+}
