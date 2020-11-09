8ba2784bdccc0a96b7e4bcff1d06d0e606909d06
Mukul <kmukul1197@gmail.com>
2020-11-09 12:31:22 +0530

    Accessing member function of abstract class using derived class object

--- Pure_virtual_function.cpp
+++ Pure_virtual_function.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Test
+/*class Test
 {
 int x;
 public:
@@ -11,4 +11,30 @@ int main(void)
 {
 Test t;     // canot create a object of pure virtual class/abstract class.
 return 0;
+}*/
+class Base{
+    public:
+    virtual void Print() = 0;
+    virtual void Display()
+    {
+    cout<<"\n\tThis is Display() method of Base Class\n";
+    }
+};
+class Derived: public Base{
+    public:
+    void Print(){
+        cout<<"Print Derived class details\n";
+    }
+    // void Display(){
+    //     cout<<"In Dispaly() of Derived class\n";
+    // }
+};
+int main(){
+    Base *ptr;
+    Derived d;  
+    d.Display();    //Member function of abstract class can be access by derived class objects
+    ptr = &d;
+    ptr->Print();
+    ptr->Display();
+    return 0;
 }
